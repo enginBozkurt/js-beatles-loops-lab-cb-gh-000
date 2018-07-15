b77b7670576be90f54a3dcd5091c9a34d595a666
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-15 10:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (var i = 0; i < 4; i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles;
+}
